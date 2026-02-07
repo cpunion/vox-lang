15091c56adf0012024b86200070a1a1dd7108b7a
Li Jie <cpunion@gmail.com>
2026-02-07 20:16:30 +0800

    stage0: stdlib sources as .vox files (embed)

--- compiler/stage0/internal/stdlib/stdlib.go
+++ compiler/stage0/internal/stdlib/stdlib.go
@@ -1,6 +1,16 @@
 package stdlib
 
-import "voxlang/internal/source"
+import (
+	_ "embed"
+
+	"voxlang/internal/source"
+)
+
+//go:embed src/std/prelude/lib.vox
+var preludeSrc string
+
+//go:embed src/std/testing/lib.vox
+var testingSrc string
 
 // Files returns stage0 stdlib sources that are implicitly available to all builds.
 //
@@ -11,32 +21,3 @@ func Files() []*source.File {
 		source.NewFile("src/std/testing/lib.vox", testingSrc),
 	}
 }
-
-const preludeSrc = `// stage0 std/prelude: keep it tiny.
-//
-// Builtins are intentionally minimal: panic/print.
-
-pub fn assert(cond: bool) -> () {
-  if !cond { panic("assertion failed"); }
-}
-
-pub fn fail(msg: String) -> () {
-  panic(msg);
-}
-
-pub fn assert_eq[T](a: T, b: T) -> () {
-  if a != b { panic("assertion failed"); }
-}
-`
-
-const testingSrc = `// stage0 std/testing: implemented in Vox, layered on std/prelude.
-
-import "std/prelude" as prelude
-
-pub fn assert(cond: bool) -> () { prelude.assert(cond); }
-
-pub fn fail(msg: String) -> () { prelude.fail(msg); }
-
-pub fn assert_eq[T](a: T, b: T) -> () { prelude.assert_eq(a, b); }
-`
-
